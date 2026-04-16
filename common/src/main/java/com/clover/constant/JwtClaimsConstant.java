af27f6c01f6a71cdf1bbfb0ab258a01f139d615c
Princess_Dragon <160112113+Princcess-Dragon@users.noreply.github.com>
2026-04-16 23:04:12 +0800

    Add files via upload

---
+++ common/src/main/java/com/clover/constant/JwtClaimsConstant.java
@@ -0,0 +1,11 @@
+package com.clover.constant;
+
+/**
+ * JWT声明常量
+ */
+public class JwtClaimsConstant {
+
+    public static final String USER_ID = "userId";
+    public static final String USERNAME = "username";
+    public static final String NICKNAME = "nickname";
+}
