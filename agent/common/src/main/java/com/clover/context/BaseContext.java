869d608b556659aac64ab4eaf750ff57d34e1856
Princess_Dragon <160112113+Princcess-Dragon@users.noreply.github.com>
2026-04-16 23:10:39 +0800

    Add files via upload

---
+++ agent/common/src/main/java/com/clover/context/BaseContext.java
@@ -0,0 +1,21 @@
+package com.clover.context;
+
+/**
+ * 线程上下文，用于保存当前登录用户ID
+ */
+public class BaseContext {
+
+    public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+
+    public static void setCurrentId(Long id) {
+        threadLocal.set(id);
+    }
+
+    public static Long getCurrentId() {
+        return threadLocal.get();
+    }
+
+    public static void removeCurrentId() {
+        threadLocal.remove();
+    }
+}
